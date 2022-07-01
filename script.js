e34e272de4f85f2058a727051234186bea1286ca
Matheus <matheuspl92@hotmail.com>
2022-07-01 16:53:34 -0300

    Add board module

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const playerFactory = (name, type) => {
+    console.log(`Player ${name} created!`);
     return {name, type, score: 0}
 }
 
+const board = (function () {
+    const boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    return {
+
+        getBoard: () => {return boardArray},
+
+        setBoard: (marker, position) => {boardArray[position] = marker},
+
+        clearBoard: () => {
+            for (let i = 0; i < boardArray.length; i++) {
+                boardArray[i] = 0;
+            }
+            console.log("BOARD CLEAR");
+        },
+        
+    };
+})()
